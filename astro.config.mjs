64cf75712f5135c29cf2c0289eb823b0e1ff1969
wonkothesaner <martin@flemingo.com>
2025-06-04 17:11:34 +0200

    change to wts.services domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://wonkothesaner.github.io',
+	site: 'https://wts.services',
 	base: '/astro-scratch-blog',
 	integrations: [mdx(), sitemap()],
 });
